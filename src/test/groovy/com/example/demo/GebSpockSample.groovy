5cc9b9567311ee41564873811ec64cc9f4a3d57d
Rachel Tung <rachtung12@gmail.com>
2018-11-20 21:57:22 +0800

    Fix test

--- src/test/groovy/com/example/demo/GebSpockSample.groovy
+++ src/test/groovy/com/example/demo/GebSpockSample.groovy
@@ -4,24 +4,32 @@ import geb.spock.GebReportingSpec
 
 class GebSpockSample extends GebReportingSpec {
 
+//    def 'Should search for Geb in Google'() {
+//        given:
+//        go "http://www.gebish.com"
+//
+//        when:
+//        Thread.sleep 1000
+//        $("input", name: "q").value("Geb Framework")
+//        Thread.sleep 1000
+//        $("#searchform").click()
+//        Thread.sleep 1000
+//        //$("input", name: "btnK").click()
+//        $("input", name: "btnG").click()
+//
+//        then:
+//        thrown(UnsupportedOperationException)
+//        waitFor { $("#search").displayed }
+//
+//        then:
+//        $("#search").text().contains("gebish.org")
+//    }
     def 'Should search for Geb in Google'() {
-        given:
-        go "http://www.google.com"
 
-        when:
-        Thread.sleep 1000
-        $("input", name: "q").value("Geb Framework")
-        Thread.sleep 1000
-        $("#searchform").click()
-        Thread.sleep 1000
-        //$("input", name: "btnK").click()
-        $("input", name: "btnG").click()
+        Browser.drive {
 
-        then:
-        thrown(UnsupportedOperationException)
-        waitFor { $("#search").displayed }
-
-        then:
-        $("#search").text().contains("gebish.org")
+            go "http://www.gebish.com"
+            assert $("h1").text() == "What is it?"
+        }
     }
 }
